0101eb828814937ddba6e564852234ccd4aca117
vanshrajput01 <rajputvansh009@gmail.com>
2024-02-22 11:45:28 +0530

    second commit

---
+++ Oop-in-Python/class.py
@@ -0,0 +1,10 @@
+class Collage:
+    def __init__(self,fname,lname):
+        self.fname = fname
+        self.lname = lname
+    
+    def studentDetails(self):
+        print(f"Student Details is{self.fname} {self.lname}.")
+
+student01 = Collage("Jack","Rones")
+print(student01.fname) 
