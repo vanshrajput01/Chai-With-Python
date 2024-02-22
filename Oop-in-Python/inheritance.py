50fec42d5e8e9d5babf598c7a19f3b625e229644
vanshrajput01 <rajputvansh009@gmail.com>
2024-02-22 14:02:03 +0530

    six commit

--- Oop-in-Python/inheritance.py
+++ Oop-in-Python/inheritance.py
@@ -14,14 +14,96 @@ class ElectricCar(Car):
     def battery_Cap(self):
         print(f"Battery capacity of this car is {self.battery}.")
 
-punch = Car("Tata","Punch")
-print(punch.brand)
-punch.carDetails()
+# punch = Car("Tata","Punch")
+# print(punch.brand)
+# punch.carDetails()
 
-tasla = ElectricCar("tasla","class s","5000KWH")
-print(tasla.model)
-tasla.battery_Cap()
-tasla.carDetails()
+# tasla = ElectricCar("tasla","class s","5000KWH")
+# print(tasla.model)
+# tasla.battery_Cap()
+# tasla.carDetails()
+        
+# Other Example based on inheritance in python
+
+class Animal:
+    def speak(self):
+        pass
+    
+class Cat(Animal):
+    def speak(self):
+        return "Meow Meow"
+
+class Dog(Animal):
+    def speak(self):
+        return "Boo Boo"
+
+
+
+cat = Cat()
+print(cat.speak())
+
+dog  = Dog()
+print(dog.speak())
+
+# Shapes Hierarchy
+
+class Shape:
+    def area(self):
+        pass
+    def perimeter(self):
+        pass
+
+class Rectangle(Shape):
+    def __init__(self,width,height):
+        self.width = width
+        self.height = height
+    
+    def area(self):
+        return self.width * self.height
+    
+    def perimeter(self):
+        return 2 * (self.width * self.height)
+
+rectangle = Rectangle(4,5)
+print(rectangle.area())
+print(rectangle.perimeter())
+
+# Employee Hierarchy:
+
+class Employee:
+    def __init__(self,name,age,salary):
+        self.name = name
+        self.age = age
+        self.salary = salary
+    
+
+class Manager(Employee):
+    def __init__(self, name, age, salary,exp_year):
+        super().__init__(name, age, salary)
+        self.exp_year = exp_year
+
+class Engineer(Employee):
+    def __init__(self, name, age, salary,stream):
+        super().__init__(name, age, salary)
+        self.stream = stream
+    
+class Intern(Employee):
+    def __init__(self, name, age, salary,durationTime):
+        super().__init__(name, age, salary)
+        self.durationTime = durationTime
+
+manager = Manager("Jack",56,90000,"10+")
+print(manager.salary)
+
+eng = Engineer("maggi",27,500000,"Full Stack dev")
+print(eng.stream)
+
+
+
+
+
+
+
     
 
         
