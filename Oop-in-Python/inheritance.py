db1dd34a941987ab5fac89305ca4122bd89dcf8d
vanshrajput01 <rajputvansh009@gmail.com>
2024-02-22 11:56:54 +0530

    first commit

---
+++ Oop-in-Python/inheritance.py
@@ -0,0 +1,22 @@
+class Car:
+    def __init__(self,brand,model):
+        self.brand = brand
+        self.model = model
+    
+    def carDetails(self):
+        print(f"Car Details is {self.brand} and {self.model}.")
+
+class ElectricCar(Car):
+    def __init__(self,brand,model,battery):
+        super.__init__(brand,model)
+        self.battery = battery
+    
+    def battery_Cap(self):
+        print(f"Battery capacity of this car is {self.battery}.")
+
+punch = Car("Tata","Punch")
+print(punch.brand)
+punch.carDetails()
+    
+
+        
