ab625c5687559b8df701e0814fa5c1678d123883
vanshrajput01 <rajputvansh009@gmail.com>
2024-02-22 14:37:07 +0530

    first commit

---
+++ Oop-in-Python/Encapsulation.py
@@ -0,0 +1,18 @@
+# Example of Encapsulation in Pyhton
+
+class Car:
+    def __init__(self,brand,model):
+        # Example of Private Variable in Class 
+        self.__brand = brand
+        self.model = model
+
+    # Getter Methrod 
+    
+    def get_brand(self):
+        print(f"Your brand car name is {self.__brand}")
+
+harrier = Car("Tata","Harrier")
+print(harrier.model)
+
+    
+
