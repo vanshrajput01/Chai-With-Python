3c83b0c12ed99abf31fd8683f8329f549649f8f6
vanshrajput01 <rajputvansh009@gmail.com>
2024-02-29 12:24:41 +0530

    instance methrod in python

---
+++ Oop-in-Python/InstanceMethrod.py
@@ -0,0 +1,14 @@
+class Person:
+    def __init__(self,fname,lname,a):
+        self.firstname = fname
+        self.lastname = lname
+        self.age = a
+
+    # instance Methrod
+    def printname(self):
+        return f'My name is {self.firstname} {self.lastname}.'
+    
+# creating object of Person Class
+p1 = Person("Uncle","Tom",56)
+print(p1.firstname)
+print(p1.printname())
