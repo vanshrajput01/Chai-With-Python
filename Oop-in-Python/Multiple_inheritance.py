6e2d21eef914f0e2638481a5534e886bdd2ea024
vanshrajput01 <rajputvansh009@gmail.com>
2024-03-03 12:50:57 +0530

    multiple inheritance in Pyhton

---
+++ Oop-in-Python/Multiple_inheritance.py
@@ -0,0 +1,36 @@
+class Person:
+    def __init__(self,fn,ln,gender,age):
+        self.fname = fn
+        self.lname = ln
+        self.gender = gender
+        self.age = age
+    
+    def walk(self,step):
+        print(f"Person Walked {step} in a day.")
+    
+class Student:
+    def __init__(self,sid,grade):
+        self.studentid = sid
+        self.grade = grade
+
+    
+    def printname(self):
+        print(f"My name is {self.fname} {self.lname}.")
+    
+class Teacher(Person,Student):
+    def __init__(self, tid,subject,fn, ln, gender, age):
+        self.teacher_id = tid
+        self.subject = subject
+        Person.__init__(self,fn, ln, gender, age)
+        
+    
+    def year_of_exp(self,year):
+        print(f"{self.fname} have more than {year}+ Exp.")
+    
+t1 = Teacher(301,"Math","Maine","Jone","Female",34)
+t1.walk(20)
+# t1.grade error
+t1.year_of_exp(10)
+t1.printname()
+
+
