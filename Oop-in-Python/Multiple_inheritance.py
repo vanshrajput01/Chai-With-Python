dc432ae82e2a5e5b8695478d90a767da9714e210
vanshrajput01 <rajputvansh009@gmail.com>
2024-03-03 15:39:12 +0530

    multiple inheritace "Final commit"

--- Oop-in-Python/Multiple_inheritance.py
+++ Oop-in-Python/Multiple_inheritance.py
@@ -18,19 +18,21 @@ class Student:
         print(f"My name is {self.fname} {self.lname}.")
     
 class Teacher(Person,Student):
-    def __init__(self, tid,subject,fn, ln, gender, age):
+    def __init__(self, tid,subject,fn, ln, gender, age,sid,grade):
         self.teacher_id = tid
         self.subject = subject
         Person.__init__(self,fn, ln, gender, age)
+        Student.__init__(self,sid,grade)
         
     
     def year_of_exp(self,year):
         print(f"{self.fname} have more than {year}+ Exp.")
     
-t1 = Teacher(301,"Math","Maine","Jone","Female",34)
+t1 = Teacher(301,"Math","Maine","Jone","Female",34,101,"B")
 t1.walk(20)
 # t1.grade error
 t1.year_of_exp(10)
 t1.printname()
+print(t1.grade)
 
 
