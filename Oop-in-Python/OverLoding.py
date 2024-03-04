818b75878b7c38ac69c7395329aed902849ea12c
vanshrajput01 <rajputvansh009@gmail.com>
2024-03-04 10:50:45 +0530

    function over loding in python

---
+++ Oop-in-Python/OverLoding.py
@@ -0,0 +1,21 @@
+#Example of normal function 
+
+def sum_of_value(value1,value2):
+    sum = value1  + value2
+    print(f"Sum of {value1} and {value2} is {sum}.")
+
+sum_of_value(10,20)
+# sum_of_value() that is give An Error.
+
+# Over loding Function in Pyhton
+
+
+def add(*args):
+    sum = 0
+    for element in args:
+        sum = sum  + element
+    print(f"Sum of your given value is {sum}.")
+
+add(1,2,3,4)  # Over loded function
+add(2,3)
+add()
