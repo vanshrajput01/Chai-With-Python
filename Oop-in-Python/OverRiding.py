c69b4219c87877713b9fac1e50b1b55837cf292e
vanshrajput01 <rajputvansh009@gmail.com>
2024-03-04 10:38:06 +0530

    Function overriding in python

--- Oop-in-Python/OverRiding.py
+++ Oop-in-Python/OverRiding.py
@@ -8,3 +8,18 @@ print_value(70)
 print(a)
 a = 80 # Overriding the variable
 print(a)
+
+# function overriding in Python
+
+def random_func():
+    pass
+
+def second_func(x,y):
+    print(f"Your give value of x is {x} and value of y is {y}.")
+
+second_func(7,6)
+
+def random_func():
+    print(f"I am example od random function in Python.")
+
+random_func()
