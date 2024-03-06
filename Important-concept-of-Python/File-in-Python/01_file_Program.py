caba7292eab453629333e5ddcffb33a3746238d0
vanshrajput01 <rajputvansh009@gmail.com>
2024-03-06 17:00:51 +0530

    four commit

--- Important-concept-of-Python/File-in-Python/01_file_Program.py
+++ Important-concept-of-Python/File-in-Python/01_file_Program.py
@@ -2,14 +2,23 @@
 
 # Read File using Python
 
-try:
-    file_name = input("Enter your file name : ")
-    f = open(file_name,"r")
-    data = f.read()
-    print(f"Your file data is : \n{data}")
+# try:
+#     file_name = input("Enter your file name : ")
+#     f = open(file_name,"r")
+#     data = f.read()
+#     print(f"Your file data is : \n{data}")
 
-except:
-    print(f"ERROR  : File is not found.")
-finally:
-    print("Your Program run success Fully.")
+# except:
+#     print(f"ERROR  : File is not found.")
+# finally:
+#     print("Your Program run success Fully.")
+
+# write the File using Python
+# w this mode overwrite the file....
+f = open("02_sample_File.txt","w")
+f.write("This is Modified File using w mode.")
+print("File write Success Fully.")
+f.close()
+
+
 
