15b1b731e2d1ffdb261eef1484b5ce0a0d0e9609
vanshrajput01 <rajputvansh009@gmail.com>
2024-03-06 20:27:00 +0530

    replace data of file

--- Important-concept-of-Python/File-in-Python/02_file_Program.py
+++ Important-concept-of-Python/File-in-Python/02_file_Program.py
@@ -1,7 +1,7 @@
 # Another syntax using to perform operation on file in Python.
 
-with open("demo.txt","r") as f:
-    print(f.read())
+# with open("demo.txt","r") as f:
+#     print(f.read())
 
 # How to delete exist file in python
     
@@ -12,6 +12,26 @@ import os
 # rename function use to rename exits file name.
 
 # os.rename("demo.txt","06_sample_File.txt")
+
+# Create A new file
+
+# Replace data of file
+
+with open("demo.txt","r") as f:
+    data = f.read()
+new_data = data.replace("Java","Python")
+print(new_data)
+
+with open("demo.txt","w") as f:
+    f.write(new_data)
+
+
+
+
+   
+
+
+
 
 
 
