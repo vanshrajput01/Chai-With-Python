49bd98f89ce107ac064b2007ccb9f63f7a865ca7
vanshrajput01 <rajputvansh009@gmail.com>
2024-03-06 16:45:45 +0530

    second commit

--- Important-concept-of-Python/File-in-Python/01_file_Program.py
+++ Important-concept-of-Python/File-in-Python/01_file_Program.py
@@ -1,2 +1,15 @@
 # File in Python
 
+# Read File using Python
+
+try:
+    file_name = input("Enter your file name : ")
+    f = open(file_name,"r")
+    data = f.read()
+    print(f"Your file data is {data}")
+
+except Exception as e:
+    print(f"ERROR : {e}")
+finally:
+    print("Your Program run success Fully.")
+
