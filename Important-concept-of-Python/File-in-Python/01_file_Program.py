6d91c660320d443bb143007a2a8c8e880f110741
vanshrajput01 <rajputvansh009@gmail.com>
2024-03-06 17:40:50 +0530

    seven commit

--- Important-concept-of-Python/File-in-Python/01_file_Program.py
+++ Important-concept-of-Python/File-in-Python/01_file_Program.py
@@ -23,11 +23,28 @@
 
 # write the File using Python
 # a this mode not overwrite the file....
-f = open("02_sample_File.txt","a")
-f.write("This file is also text file.")
-print("File write success Fully.")
+# f = open("02_sample_File.txt","a")
+# f.write("This file is also text file.")
+# print("File write success Fully.")
+# f.close()
+# print("File close success Fully.")
+
+# r+ mode in file : Read & Write
+
+f = open("03_sample_File.txt","r+")
+f.write("abd file") # Over write and add data from begin
+print(f.read()) # in this mode complete data is not printed of the file.
 f.close()
-print("File close success Fully.")
+
+# w+ mode in file : Read & write
+
+
+
+
+
+
+
+
 
 
 
