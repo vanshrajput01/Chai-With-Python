a667ee740d2be0b8a8b37429d168119bbf9464bd
vanshrajput01 <rajputvansh009@gmail.com>
2024-03-06 16:37:39 +0530

    final commit

--- Important-concept-of-Python/Exception_Hanling.py
+++ Important-concept-of-Python/Exception_Hanling.py
@@ -1,15 +1,15 @@
 # Exception handing in Python
-# try:
-#     a = int(input("Enter the value of a : "))
-#     b = int(input("Enter value of b : "))
-#     result = a / b
-#     print(f"Your result is {result}.")
+try:
+    a = int(input("Enter the value of a : "))
+    b = int(input("Enter value of b : "))
+    result = a / b
+    print(f"Your result is {result}.")
 
-# except Exception as e:
-#     print(f"Error : {e}")
+except Exception as e:
+    print(f"Error : {e}")
 
-# finally:
-#     print("Your Program run successfully")
+finally:
+    print("Your Program run successfully")
 
 
 # Program 02
